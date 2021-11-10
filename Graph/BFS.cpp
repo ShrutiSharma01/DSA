ef802cc56fd1653c5018ff329ad0db7a929af295
ShrutiSharma01 <81433585+ShrutiSharma01@users.noreply.github.com>
2021-11-10 14:08:42 +0530

    bfs.cpp

--- Graph/BFS.cpp
+++ Graph/BFS.cpp
@@ -1,28 +1,42 @@
-#define pii pair<int, int>
+#include <iostream>
+#include <vector>
 
-vector<vector<int>> graph;
-
-vector<int> bfs(int n, int source)
-{
-    vector<bool> visited(n, false);
-    vector<int> dist(n, INT32_MAX);
-
+vector<int> bfs(int u, vector<vector<int> > &adj, vector<bool> &visited, vector<int> dist) {
     queue<int> q;
-    q.push(source);
-    visited[source] = true;
-    dist[source] = 0;
-    while (!q.empty())
-    {
-        int v = q.front();
+    q.push(0);
+    dist[0] = 0;
+    while (!q.empty()) {
+        int temp = q.front();
+        visited[temp] = true;
+        cout << temp + 1 << endl;
         q.pop();
-        for (auto u : graph[v])
-            if (!visited[u])
-            {
-                q.push(u);
-                visited[u] = true;
-                dist[u] = dist[v] + 1;
+        for (auto i: adj[temp]) {
+            if(!visited[i]) {
+                q.push(i);
+                visited[i] = true;
+                dist[i] = dist[temp] + 1;
             }
+        }
+    }
+    return dist;
+}
+
+int main() {
+    int nodes, edges, u, v;
+    cin >> nodes >> edges;
+    vector<vector<int> > adj(nodes);
+    vector<bool> visited(nodes, false);
+    vector<int> dist(nodes);
+    for(int i = 0; i <= edges; ++i) {
+        cin >> u >> v;
+        --u, --v;       
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
-    return dist;
+    vector<int> distances = bfs(0, adj, visited, dist);   //assume root is always node 0
+    for (int i = 0; i < nodes; i++) {
+        cout << "Distance of " << i + 1 << " is " << distances[i] << endl;
+    }
+    return 0;
 }
